9a24ab9c6d9604c35a2ef24abce7fd639ee8c744
Salvatore Olivieri <99086797+salvatoreolivieri@users.noreply.github.com>
2023-05-28 13:40:58 +0200

    Add Queue implementation using Stacks

--- Queues.js
+++ Queues.js
@@ -70,4 +70,51 @@ myQueue.enqueue("Matt")
 myQueue.enqueue("Pavel")
 myQueue.enqueue("Samir")
 myQueue.dequeue()
+myQueue.dequeue()
+myQueue.dequeue()
+myQueue.dequeue()
 myQueue.isEmpty()
+
+// Implement Queue using Stacks
+
+class CrazyQueue {
+  constructor() {
+    this.first = []
+    this.last = []
+  }
+
+  enqueue(value) {
+    const length = this.first.length
+    for (let i = 0; i < length; i++) {
+      this.last.push(this.first.pop())
+    }
+    this.last.push(value)
+    return this
+  }
+
+  dequeue() {
+    const length = this.last.length
+    for (let i = 0; i < length; i++) {
+      this.first.push(this.last.pop())
+    }
+    this.first.pop()
+    return this
+  }
+  peek() {
+    if (this.last.length > 0) {
+      return this.last[0]
+    }
+    return this.first[this.first.length - 1]
+  }
+}
+
+const myCrazyQueue = new CrazyQueue()
+myCrazyQueue.peek()
+myCrazyQueue.enqueue("Joy")
+myCrazyQueue.enqueue("Matt")
+myCrazyQueue.enqueue("Pavel")
+myCrazyQueue.peek()
+myCrazyQueue.dequeue()
+myCrazyQueue.dequeue()
+myCrazyQueue.dequeue()
+myCrazyQueue.peek()

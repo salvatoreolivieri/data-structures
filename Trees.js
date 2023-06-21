9c9cfcdd7746984e6ceddaaedd68f7be3013bd7d
Salvatore Olivieri <99086797+salvatoreolivieri@users.noreply.github.com>
2023-06-21 17:12:38 +0200

    Add breadthFirstSearch

--- Trees.js
+++ Trees.js
@@ -16,27 +16,27 @@ class BinarySearchTree {
   insert(value) {
     const newNode = new Node(value)
 
-    if (!this.root) {
+    if (this.root === null) {
       this.root = newNode
-    }
+    } else {
+      let currentNode = this.root
 
-    let currentNode = this.root
-
-    while (true) {
-      // Left
-      if (value < currentNode.value) {
-        if (!currentNode.left) {
-          currentNode.left = newNode
-          return this
+      while (true) {
+        if (value < currentNode.value) {
+          //Left
+          if (!currentNode.left) {
+            currentNode.left = newNode
+            return this
+          }
+          currentNode = currentNode.left
+        } else {
+          //Right
+          if (!currentNode.right) {
+            currentNode.right = newNode
+            return this
+          }
+          currentNode = currentNode.right
         }
-        currentNode = currentNode.left
-      } else {
-        // Right
-        if (!currentNode.right) {
-          currentNode.right = newNode
-          return this
-        }
-        currentNode = currentNode.right
       }
     }
   }
@@ -59,18 +59,67 @@ class BinarySearchTree {
     }
     return false
   }
+
   remove() {}
+
+  breadthFirstSearch() {
+    let currentNode = this.root
+    let list = []
+    let queue = []
+    queue.push(currentNode)
+
+    while (queue.length > 0) {
+      currentNode = queue.shift()
+
+      list.push(currentNode.value)
+
+      if (currentNode.left) {
+        queue.push(currentNode.left)
+      }
+
+      if (currentNode.right) {
+        queue.push(currentNode.right)
+      }
+
+      console.log(list)
+
+      return list
+    }
+  }
+
+  breadthFirstSearchRecursive(queue, list) {
+    if (!queue.length) {
+      console.log(list)
+
+      return list
+    }
+
+    let currentNode = queue.shift()
+    list.push(currentNode.value)
+
+    if (currentNode.left) {
+      queue.push(currentNode.left)
+    }
+
+    if (currentNode.right) {
+      queue.push(currentNode.right)
+    }
+
+    return this.breadthFirstSearchRecursive(queue, list)
+  }
 }
 
 const tree = new BinarySearchTree()
 
 tree.insert(9)
 tree.lookup(9)
-// tree.insert(4)
-// tree.insert(6)
-// tree.insert(20)
-// tree.insert(170)
-// tree.insert(15)
-// tree.insert(1)
+tree.insert(4)
+tree.insert(6)
+tree.insert(20)
+tree.insert(170)
+tree.insert(15)
+tree.insert(1)
+tree.breadthFirstSearch()
+tree.breadthFirstSearchRecursive([tree.root], [])
 
 console.log(tree)
